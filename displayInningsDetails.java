c9e3486f46ab888e5d6812141fd74941c569c8c2
kricha-24 <78844495+kricha-24@users.noreply.github.com>
2021-03-03 08:53:41 +0530

    Update displayInningsDetails.java

--- displayInningsDetails.java
+++ displayInningsDetails.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 class displayInningsDetails{  
 public static void main(String[] args){
-	isplayInningsDetails I = new isplayInningsDetails();
+	displayInningsDetails I = new displayInningsDetails();
 	System.out.println("Enter the team name: ");
-    I.setTeamName = (input.nextLine());
+	I.setTeamName = (input.nextLine());
 	System.out.println("Enter session: ");
 	I.setInningsName = (Input.nextLine());
 	System.out.println("Enter runs: ");
